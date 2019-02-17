c3c742e66fff0fc13f8ebdecd4b657c06717dba0
proway2 <sixflags@yandex.ru>
2019-02-17 13:46:17 +0300

    Добавлены тесты для замыкания - обработчика URL.

--- kvserver_test.go
+++ kvserver_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	"kvserver/kvstorage"
 	"net/http"
+	"net/url"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -119,3 +121,162 @@ func Test_getKeyFromURL(t *testing.T) {
 // 		})
 // 	}
 // }
+
+type myResponseWriter struct {
+	code int
+}
+
+func (resp *myResponseWriter) Header() http.Header {
+	res := make(map[string][]string)
+	return res
+}
+
+func (resp *myResponseWriter) WriteHeader(code int) {
+	resp.code = code
+}
+
+func (resp *myResponseWriter) Write(inp []byte) (int, error) {
+	var er error
+	return int(1), er
+}
+
+func (resp *myResponseWriter) getCode() int {
+	return resp.code
+}
+
+func Test_closure(t *testing.T) {
+	storage := kvstorage.KVStorage{}
+	chanOut := make(chan string, 10)
+	storage.Init(&chanOut)
+
+	handler := getHandler(&storage)
+	var writer *myResponseWriter
+	writer = &myResponseWriter{code: 200}
+
+	form := url.Values{}
+	form.Add("value", "key111 test")
+	reqSet, _ := http.NewRequest("POST", "http://localhost:8080/key/key111", strings.NewReader(form.Encode()))
+	reqSet.Header.Add("value", "key111 test")
+	reqSet.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	form = url.Values{}
+	form.Add("valu", "key111 test")
+	reqBad, _ := http.NewRequest("POST", "http://localhost:8080/key/key111", strings.NewReader(form.Encode()))
+	reqBad.Header.Add("valu", "key111 test")
+	reqBad.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+		want int // код ошибки
+		text string
+	}{
+		{
+			name: "Получение из пустого хранилища",
+			args: args{
+				w: writer,
+				r: &http.Request{
+					Method: "GET",
+					URL: &url.URL{
+						Scheme: "http",
+						Host:   "localhost:8080",
+						Path:   "/key/key111",
+					},
+				},
+			},
+			want: 404,
+		},
+		{
+			name: "Удаление из пустого хранилища",
+			args: args{
+				w: writer,
+				r: &http.Request{
+					Method: "POST",
+					URL: &url.URL{
+						Scheme: "http",
+						Host:   "localhost:8080",
+						Path:   "/key/key111",
+					},
+				},
+			},
+			want: 404,
+		},
+		{
+			name: "Пустое значение ключа",
+			args: args{
+				w: writer,
+				r: &http.Request{
+					Method: "POST",
+					URL: &url.URL{
+						Scheme: "http",
+						Host:   "localhost:8080",
+						Path:   "/key/",
+					},
+				},
+			},
+			want: 400,
+		},
+		{
+			name: "Короткое значение ключа",
+			args: args{
+				w: writer,
+				r: &http.Request{
+					Method: "POST",
+					URL: &url.URL{
+						Scheme: "http",
+						Host:   "localhost:8080",
+						Path:   "/ke/",
+					},
+				},
+			},
+			want: 400,
+		},
+		{
+			name: "Установка значения",
+			args: args{
+				w: writer,
+				r: reqSet,
+			},
+			want: 200,
+		},
+		{
+			name: "Удаление существующего значения",
+			args: args{
+				w: writer,
+				r: &http.Request{
+					Method: "POST",
+					URL: &url.URL{
+						Scheme: "http",
+						Host:   "localhost:8080",
+						Path:   "/key/key111",
+					},
+				},
+			},
+			want: 200,
+		},
+		{
+			name: "Некорректный URL для установки значения",
+			args: args{
+				w: writer,
+				r: reqBad,
+			},
+			want: 400,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// сброс кода ответа
+			tt.args.w.WriteHeader(200)
+
+			handler(tt.args.w, tt.args.r)
+			got := writer.getCode()
+			if got != tt.want {
+				t.Errorf("urlHandler() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
