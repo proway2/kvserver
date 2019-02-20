35651688f9ce96c9e2a6b334fe1606daf6a6df0c
proway2 <sixflags@yandex.ru>
2019-02-20 13:37:14 +0300

    Функции обработчики URL вынесены в пакет router.

--- kvserver.go
+++ kvserver.go
@@ -2,91 +2,13 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"kvserver/kvstorage"
+	"kvserver/router"
 	"kvserver/vacuum"
 	"log"
 	"net/http"
 	"strconv"
 )
-
-// замыкание необходимо для оборачивания локальных переменных в обработчик URL
-func getURLHandler(stor *kvstorage.KVStorage) func(
-	w http.ResponseWriter, r *http.Request,
-) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		keyName, ok := getKeyFromURL(r.URL.Path)
-		if !ok {
-			w.WriteHeader(400) // Bad request
-			fmt.Fprint(w, "400 Malformed request.\n")
-			return
-		}
-		reqHandler, isHandlerExists := creator(r.Method)
-		if !isHandlerExists {
-			return
-		}
-		val, code := reqHandler(stor, keyName, r)
-		w.WriteHeader(code)
-		fmt.Fprint(w, val)
-	}
-}
-
-func getKeyFromURL(inps string) (string, bool) {
-	if len(inps) < 6 {
-		return "", false
-	}
-	return inps[5:], true
-}
-
-// Фабричная функция
-func creator(method string) (func(*kvstorage.KVStorage, string, *http.Request) (string, int), bool) {
-	if method == "GET" {
-		return GEThandler, true
-	}
-	if method == "POST" {
-		return POSThandler, true
-	}
-	return nil, false
-}
-
-// GEThandler - функция обработчика метода GET
-func GEThandler(stor *kvstorage.KVStorage, key string, r *http.Request) (string, int) {
-	code := 200
-	// Получаем значение по ключу
-	val, res := stor.Get(key)
-	if !res {
-		// значение ключа не найдено - ошибка 404
-		val = "404 There is no record in storage for this key.\n"
-		code = 404
-	}
-	return val, code
-}
-
-// POSThandler - функция обработчика метода POST
-func POSThandler(stor *kvstorage.KVStorage, key string, r *http.Request) (string, int) {
-	// требуется для извлечения значений метода POST - заполняется r.Form
-	val := ""
-	code := 200
-	r.ParseForm()
-	if len(r.Form) == 0 {
-		// удаление значения по ключу
-		if !stor.Delete(key) {
-			// значение не удалено
-			code = 404
-			val = "404 There is no record in storage for this key.\n"
-		}
-	} else {
-		if value, ok := r.Form["value"]; ok {
-			// требуется установка значения
-			stor.Set(key, value[0])
-		} else {
-			// Bad request
-			code = 400
-			val = "400 Malformed request.\n"
-		}
-	}
-	return val, code
-}
 
 func getCLIargs() (string, int, uint64) {
 	ttlP := flag.Uint64(
@@ -134,7 +56,7 @@ func main() {
 	server := &http.Server{
 		Addr: addr + ":" + strconv.Itoa(port),
 	}
-	urlHandler := getURLHandler(storage)
+	urlHandler := router.GetURLrouter(storage)
 
 	// для работы веб-сервера требуется определить обработчик URL
 	http.HandleFunc("/key/", urlHandler)
