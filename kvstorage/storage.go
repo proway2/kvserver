d1381494bbf01e9ebcde4d8a6826e7b0f4250065
proway2 <sixflags@yandex.ru>
2019-03-02 19:43:38 +0300

    Storage is completely redesigned and simplified.

--- kvstorage/storage.go
+++ kvstorage/storage.go
@@ -2,6 +2,7 @@ package kvstorage
 
 import (
 	"container/list"
+	"errors"
 	"kvserver/element"
 	"sync"
 	"time"
@@ -11,12 +12,15 @@ import (
 type KVStorage struct {
 	kvstorage   map[string]*element.Element
 	mux         sync.Mutex
-	queue       list.List
+	queue       list.List // LIFO - the oldest element is always at the front!!!
 	initialized bool
 }
 
 // Init - Функция инициализации хранилища
-func (kv *KVStorage) Init(out chan string) bool {
+func (kv *KVStorage) Init() bool {
+	if kv.initialized {
+		return false
+	}
 	kv.kvstorage = make(map[string]*element.Element)
 	kv.initialized = true
 	return true
@@ -28,23 +32,28 @@ func (kv *KVStorage) Set(key, value string) bool {
 		return false
 	}
 	kv.mux.Lock()
+	defer kv.mux.Unlock()
+
 	// проверяем есть ли у нас такой ключ в карте
-	_, found := kv.kvstorage[key]
-	if found {
-		// это точно должно быть здесь?
+	if elem, found := kv.kvstorage[key]; found {
+		// для поддержания порядка очереди LIFO,
+		// надо удалить найденный элемент из очереди
+		// вместо него будет новый с таким же ключом
+		kv.queue.Remove(elem.QueueElement)
 	}
-	// ключ есть, надо обновить его значение и установить признак updated
+	// in order to maintain LIFO new elements push back
 	elem := &element.Element{
-		Val: value, Timestamp: time.Now(),
+		Val:          value,
+		Timestamp:    time.Now(),
+		QueueElement: kv.queue.PushBack(key),
 	}
 	kv.kvstorage[key] = elem
-	kv.mux.Unlock()
 
 	return true
 }
 
 // Get - получение значения по ключу,
-// второй параметр указывает на успешность получения значения по ключу
+// второе возвращаемое значение указывает на успешность получения значения по ключу
 func (kv *KVStorage) Get(key string) (string, bool) {
 	if !kv.initialized || len(key) == 0 {
 		return "", false
@@ -58,21 +67,29 @@ func (kv *KVStorage) Get(key string) (string, bool) {
 	return "", false
 }
 
-// GetTimestamp - получить метку времени элемента
-// func (kv *KVStorage) GetTimestamp(key string) int64 {
-// 	if !kv.initialized || len(key) == 0 {
-// 		return 0
-// 	}
-// 	kv.mux.Lock()
-// 	defer kv.mux.Unlock()
-// 	elem, ok := kv.kvstorage[key]
-// 	if ok {
-// 		return elem.Timestamp
-// 	}
-// 	return 0
-// }
+// OldestElementTime - получить метку времени старейшего элемента
+func (kv *KVStorage) OldestElementTime() (time.Time, error) {
+	if !kv.initialized {
+		return time.Time{}, errors.New("Storage is not initialized.")
+	}
+	kv.mux.Lock()
+	defer kv.mux.Unlock()
 
-// Delete - удаление значения по ключу
+	oldestElemInQueue := kv.queue.Front()
+	if oldestElemInQueue == nil {
+		return time.Time{}, errors.New("Not found in storage.")
+	}
+
+	key := oldestElemInQueue.Value.(string)
+	elem, ok := kv.kvstorage[key]
+	if ok {
+		return elem.Timestamp, nil
+	}
+	// WTF? Element is in queue, but not in the map?!
+	return time.Time{}, errors.New("Shit happens!")
+}
+
+// Delete - delete element from storage by its key
 func (kv *KVStorage) Delete(key string) bool {
 	if !kv.initialized || len(key) == 0 {
 		return false
@@ -81,59 +98,42 @@ func (kv *KVStorage) Delete(key string) bool {
 	defer kv.mux.Unlock()
 	_, ok := kv.kvstorage[key]
 	if ok {
-		delete(kv.kvstorage, key)
+		kv.purgeElement(key)
 	}
 	return ok
 }
 
-// ResetUpdated - сброс признака обновления элемента в false
-// func (kv *KVStorage) ResetUpdated(key string) bool {
-// 	if !kv.initialized || len(key) == 0 {
-// 		return false
-// 	}
-// 	kv.mux.Lock()
-// 	defer kv.mux.Unlock()
-// 	if elem, ok := kv.kvstorage[key]; ok {
-// 		elem.Updated = false
-// 		return true
-// 	}
-// 	return false
-// }
-
-// IsElemUpdated - получение признака был ли элемент обновлен
-// func (kv *KVStorage) IsElemUpdated(key string) bool {
-// 	if !kv.initialized || len(key) == 0 {
-// 		return false
-// 	}
-// 	kv.mux.Lock()
-// 	defer kv.mux.Unlock()
-// 	elem, ok := kv.kvstorage[key]
-// 	if ok {
-// 		return elem.Updated
-// 	}
-// 	return ok
-// }
-
-// IsElemTTLOver - функция проверяет должен ли элемент быть удален
-// func (kv *KVStorage) IsElemTTLOver(key string, ttl uint64) bool {
-// 	if !kv.initialized || len(key) == 0 {
-// 		return false
-// 	}
-// 	kv.mux.Lock()
-// 	defer kv.mux.Unlock()
-// 	if elem, ok := kv.kvstorage[key]; ok {
-// 		return elem.IsTTLOver(ttl)
-// 	}
-// 	return true
-// }
-
-// IsInStorage - проверка нахождения элемента с данным ключем в хранилище
-func (kv *KVStorage) IsInStorage(key string) bool {
-	if !kv.initialized || len(key) == 0 {
+// DeleteFrontIfOlder - removes front element if its older than ctxTime
+func (kv *KVStorage) DeleteFrontIfOlder(ctxTime time.Time) bool {
+	if !kv.initialized {
 		return false
 	}
 	kv.mux.Lock()
 	defer kv.mux.Unlock()
-	_, ok := kv.kvstorage[key]
-	return ok
+
+	// first need to check if there is something in the queue
+	oldestElemInQueue := kv.queue.Front()
+	if oldestElemInQueue == nil {
+		return false
+	}
+
+	// map key is needed to test the element against input time
+	key := oldestElemInQueue.Value.(string)
+	// need to check if it's in storage.
+	// it MUST be in storage at this point
+	elem, _ := kv.kvstorage[key]
+	if elem.Timestamp.Before(ctxTime) {
+		kv.purgeElement(key)
+		return true
+	}
+	return false
+}
+
+func (kv *KVStorage) purgeElement(key string) {
+	// THIS IS NOT THREAD SAFE FUNCTION !!!
+	// INTERNAL USE ONLY !!!
+	// CALL THIS FUNCTION WITHIN CRITICAL SECTION
+	// WHEN THREAD IS LOCKED !!!
+	kv.queue.Remove(kv.kvstorage[key].QueueElement)
+	delete(kv.kvstorage, key)
 }
